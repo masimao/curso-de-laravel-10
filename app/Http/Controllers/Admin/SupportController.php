3b80857668ae5a148e8baaeb0a7b2fef59aaf923
Marcio Mazzucato <marcio@evomaster.com.br>
2023-08-17 22:37:57 -0300

    CRUD completed

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -38,4 +38,37 @@ class SupportController extends Controller
 
         return redirect()->route('supports.index');
     }
+
+    public function edit(Support $support, string|int $id)
+    {
+        if (!$support = $support->where('id', $id)->first()) {
+            return back();
+        }
+
+        return view('admin/supports/edit', compact('support'));
+    }
+
+    public function update(Request $request, Support $support, string|int $id)
+    {
+        if (!$support = $support->find($id)) {
+            return back();
+        }
+
+        $support->update($request->only([
+            'subject', 'body'
+        ]));
+
+        return redirect()->route('supports.index');
+    }
+
+    public function destroy(string|int $id)
+    {
+        if (!$support = Support::find($id)) {
+            return back();
+        }
+
+        $support->delete();
+
+        return redirect()->route('supports.index');
+    }
 }
--- resources/views/admin/supports/show.blade.php
+++ resources/views/admin/supports/show.blade.php
@@ -5,3 +5,9 @@
     <li>{{ $support->status }}</li>
     <li>{{ $support->body }}</li>
 </ul>
+
+<form action="{{ route('supports.destroy', $support->id) }}" method="POST">
+    @csrf()
+    @method('delete')
+    <button type="submit">Deletar</button>
+</form>
